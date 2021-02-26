2143f0cbcfc012ee329ccdefe5f749e2d53889d6
Diello Arias <dielloarias@gmail.com>
2021-02-26 10:59:54 -0300

    small enhancements

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -1,13 +1,20 @@
+export interface AuthInfo {
+    host: string;
+    user: string;
+    password: string;
+    database: string;
+}
+
 export interface CharacterInfo {
-	date?: any;
-	currentBid?: number;
-	auctionStart?: Date | string;
-	auctionEnd?: Date | string;
-	characterName?: string;
-	level?: number;
-	vocation?: string;
-	sex?: string;
-	world?: string;
-	obs?: string;
-	sold?: string | boolean;
+    date?: any;
+    currentBid?: number;
+    auctionStart?: Date | string;
+    auctionEnd?: Date | string;
+    characterName?: string;
+    level?: number;
+    vocation?: string;
+    sex?: string;
+    world?: string;
+    obs?: string;
+    sold?: string | boolean;
 }

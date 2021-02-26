4e50601d43b7f0c6d87c7997d1b44a9fbc754a51
Diello Arias <dielloarias@gmail.com>
2021-02-26 11:04:15 -0300

    small enhancements

---
+++ src/db/conexoes/auth/Auth.ts
@@ -0,0 +1,9 @@
+import { AuthInfo } from "../../../Interfaces";
+
+export const Auth: AuthInfo = {
+    // Utilizar suas configurações de banco de dados; código desenvolvido para rodar c/ MySQL e MariaDB
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "DB_TIBIA_AUCTIONS",
+};
